6c8c1d806fe7cd57826a86a3de753f7382686498
Andi Ahmad Yusup <andiahmadyusup@gmail.com>
2024-05-20 07:31:44 +0700

    Birthday Cake Candles

---
+++ birthday-cake-candles/script.js
@@ -0,0 +1,14 @@
+/**
+ * 1. Ah ini disuruh hitung banyak total angka paling tinggi ya
+ * 2. Sort dulu ga sih, nanti ambil index dari angka paling tinggi abis tuh splice
+ * 3. Let's goo
+ */
+function birthdayCakeCandles(candles) {
+    candles.sort((a, b) => a - b);
+
+    const max = Math.max(...candles);
+    const tallestCandles = candles.slice(candles.indexOf(max), candles.lastIndexOf(max) + 1)
+
+    console.log(tallestCandles.length);
+}
+
